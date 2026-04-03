4e0e4bc3c7e1a2fd0ba3e90fa3a71a2564a7d706
yugene-rhapsody <yugene@adrop.io>
2026-04-03 14:55:26 +0900

    chore: remove intentional failure test

--- apps/console/tests/fail-on-purpose.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test, expect } from '@playwright/test'
-
-
-test('이 테스트는 일부러 실패합니다', async ({ page }) => {
-    await page.goto('https://playwright.dev/')
-    await expect(page).toHaveTitle('이건 절대 안 맞는 제목')
-})
